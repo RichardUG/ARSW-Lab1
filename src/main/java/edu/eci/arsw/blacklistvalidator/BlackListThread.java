e425edc21ae5640fa8a51c6cca8b9640360cc0fa
Alejandro Toro Daza <skullzo@outlook.com>
2021-01-23 14:27:10 -0500

    Código Fuente

--- src/main/java/edu/eci/arsw/blacklistvalidator/BlackListThread.java
+++ src/main/java/edu/eci/arsw/blacklistvalidator/BlackListThread.java
@@ -1,40 +1,44 @@
 package edu.eci.arsw.blacklistvalidator;
-
+import java.util.LinkedList;
 import edu.eci.arsw.spamkeywordsdatasource.HostBlacklistsDataSourceFacade;
-
-import java.util.concurrent.CopyOnWriteArrayList;
-import java.util.concurrent.atomic.AtomicInteger;
 
 public class BlackListThread extends Thread{
     private static final int BLACK_LIST_ALARM_COUNT=5;
-    private int start_server;
-    private int final_server;
-    private String ipAddress;
-    private AtomicInteger ocurrencesCount;
-    private AtomicInteger checkedListsCount;
-    private HostBlacklistsDataSourceFacade skds;
-    private CopyOnWriteArrayList<Integer> blackListOcurrence;
-
-    public BlackListThread(int start, int f_server, String ipAddress, AtomicInteger ocurrencesCount, CopyOnWriteArrayList<Integer> blackListOcurrence, HostBlacklistsDataSourceFacade skds, AtomicInteger checkedListsCount) {
-        this.start_server = start;
-        this.final_server = f_server;
-        this.ipAddress = ipAddress;
-        this.ocurrencesCount = ocurrencesCount;
-        this.blackListOcurrence = blackListOcurrence;
-        this.checkedListsCount = checkedListsCount;
-        this.skds = skds;
-
-    }
-
-    @Override
-    public void run() {
-        for (int i = start_server; i < final_server && ocurrencesCount.get() < BLACK_LIST_ALARM_COUNT; i++) {
-            checkedListsCount.getAndIncrement();
-            if (skds.isInBlackListServer(i, ipAddress)) {
-                blackListOcurrence.add(i);
-                ocurrencesCount.getAndIncrement();
+	private int inicio;
+	private int fin;
+	private String Host;
+    HostBlacklistsDataSourceFacade skds=HostBlacklistsDataSourceFacade.getInstance();
+    LinkedList<Integer> blackListOcurrences=new LinkedList<>();
+    private int checkedListsCount;
+    private int ocurrencesCount;
+    
+	public BlackListThread(int i,int f,String Host) { 
+		this.inicio=i;
+		this.fin=f;
+		this.Host=Host;
+		this.checkedListsCount = 0;
+		this.ocurrencesCount = 0;
+	}
+	
+	public void run() {
+        for (int i=inicio;i<fin && ocurrencesCount<BLACK_LIST_ALARM_COUNT;i++){
+            checkedListsCount++;
+            if (skds.isInBlackListServer(i, Host)) {
+                blackListOcurrences.add(i);
+                ocurrencesCount++;
             }
-        }
-    }
-
+        }   
+	}
+	
+	public LinkedList<Integer> getBlackListOcurrences() {
+		return blackListOcurrences;
+	}
+	
+	public int getCheckedListsCount() {
+		return checkedListsCount;
+	}
+	
+	public int getOcurrencesCount() {
+		return ocurrencesCount;
+	}
 }
